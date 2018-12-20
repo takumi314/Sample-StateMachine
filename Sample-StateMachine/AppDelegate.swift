56ae4f998b88a0f014b918d31e6704e85003b331
takumi314 <isospin_symmetry@me.com>
2018-12-21 00:52:57 +0900

    Initial commit

---
+++ Sample-StateMachine/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Sample-StateMachine
+//
+//  Created by NishiokaKohei on 21/12/2018.
+//  Copyright © 2018 Takumi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
